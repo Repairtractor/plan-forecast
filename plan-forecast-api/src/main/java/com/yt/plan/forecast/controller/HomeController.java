4c1203b995f1f9cd612a14f82811d51beb4ba152
lcc <13197149229@163.com>
2023-07-04 17:16:48 +0800

    vscode的一次测试提交

--- plan-forecast-api/src/main/java/com/yt/plan/forecast/controller/HomeController.java
+++ plan-forecast-api/src/main/java/com/yt/plan/forecast/controller/HomeController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class HomeController {
     @GetMapping("")
-    public String home() {        
+    public String home() {    
+        System.out.println("Hello World!,这是为了测试提交");    
         return "Hello World!";
     }
 }
